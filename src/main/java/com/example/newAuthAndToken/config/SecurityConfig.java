b533bad14403feb81a05c0357118107a67491ad9
omarali18 <omar.eu18@gmail.com>
2026-01-29 12:10:16 +0600

    first commit

---
+++ src/main/java/com/example/newAuthAndToken/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.newAuthAndToken.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(section -> section.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+        //        http.formLogin(Customizer.withDefaults());
+
+//        return http.build();
+    }
+}
---
+++ src/main/java/com/example/newAuthAndToken/entity/Student.java
@@ -0,0 +1,59 @@
+package com.example.newAuthAndToken.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int marks;
+
+
+    public Student(long id,
+                   String name,
+                   int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
+
